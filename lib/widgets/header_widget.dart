ab8533862f03c36f799d2e0fda4c0f49b3a58140
al2903e <latz@fh-aachen.de>
2023-07-14 10:36:54 +0200

    centered header

--- lib/widgets/header_widget.dart
+++ lib/widgets/header_widget.dart
@@ -7,23 +7,23 @@ class HeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
+    return Stack(
+      //  mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Align(
-          alignment: Alignment.topLeft,
-          child: IconButton(
-            constraints: const BoxConstraints(),
-            padding: EdgeInsets.zero,
-            icon: const Icon(Icons.menu, color: Colors.white),
-            onPressed: () => scaffoldKey.currentState!.openDrawer(),
-          ),
+        IconButton(
+          constraints: const BoxConstraints(),
+          padding: EdgeInsets.zero,
+          icon: const Icon(Icons.menu, color: Colors.white),
+          onPressed: () => scaffoldKey.currentState!.openDrawer(),
         ),
         Align(
-          alignment: Alignment.center,
-          child: Text('DAILY DOSE OF HAPPINESS',
-              style: AppTextStyle.getHeaderTextStyle(
-                  Colors.white.withOpacity(0.6))),
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Text('DAILY DOSE OF HAPPINESS',
+                style: AppTextStyle.getHeaderTextStyle(
+                    Colors.white.withOpacity(0.6))),
+          ),
         ),
       ],
     );
