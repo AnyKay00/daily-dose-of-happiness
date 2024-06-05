e6f313869025c36eef822f35ffe905e1dfb527e6
al2903e <latz@fh-aachen.de>
2024-06-05 12:10:04 +0200

    repaired gradle

--- android/app/src/main/kotlin/com/example/daily_dose_of_happiness/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.daily_dose_of_happiness
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/invio/dailydoseofhappiness/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.invio.dailydoseofhappiness
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
